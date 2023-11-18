e2bd1ed0e8086d14b288bdcf2a16b2d44e608601
sevase <kotaseva8@gmail.com>
2023-11-18 14:59:11 +0300

    Добавьте файлы проекта.

---
+++ DetSad/Classes/OpenWindowClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DetSad.Classes
+{
+    public class OpenWindowClass
+    {
+        public static void OpenWindow<T>() where T : Window, new()
+        {
+            Window wind = new T();
+            wind.Show();
+        }
+    }
+}
---
+++ DetSad/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using DetSad.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DetSad.Windows;
+
+namespace DetSad
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ControlClass cntrlCl = new ControlClass(this);
+            Btn_Close.Click += cntrlCl.close_control;
+            Btn_minim.Click += cntrlCl.minimized_control;
+            Btn_perezapusk.Click += (sender, e) => cntrlCl.perezapusk_control(new MainWindow());
+            br_up.MouseLeftButtonDown += cntrlCl.Window_MouseLeftButtonDown;
+            br_up.MouseMove += cntrlCl.Window_MouseMove;
+        }
+
+        private void WindowAuth_Click(object sender, RoutedEventArgs e)
+        {
+            var main = GetWindow(this) as MainWindow;
+            OpenWindowClass.OpenWindow<WindowAuthWorker>();
+            main.Close();
+        }
+    }
+}
